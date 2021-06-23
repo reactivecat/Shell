089adbb57f6fdb215f9a099c1b576457c4e3c769
gaizi <chenjianjun@yiwei.com>
2021-06-23 19:21:40 +0800

    xxxxxxxxxxxxxxxxx

--- git-commit-checker.sh
+++ git-commit-checker.sh
@@ -2,7 +2,7 @@
 echo "hello,shell script!"
 commit_msg="llalal,cmomit"
 echo "${commit_msg},melo girl!"
-echo "????$0 $1 $2 $3 hello"
+echo "????$0 == $1  === $2  == $3 hello"
 echo 'args:---- '$0''
 commit_message=`cat ../../$1`
 echo  "content of commit_message: $commit_message lllllllll"
