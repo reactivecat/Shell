fbd4bcaa952b1874bed8a4b1360d99c4e79c121c
gaizi <chenjianjun@yiwei.com>
2021-06-23 19:47:46 +0800

    xxxxxxxxxxxxxxxxx

--- git-commit-checker.sh
+++ git-commit-checker.sh
@@ -1,3 +1,10 @@
-#!/bin/bash
-commit_msg=$(cat "${$1:?Missing commit message file}")
-echo "$commit_msg ------>"
+ SOB=$(git var GIT_AUTHOR_IDENT | sed -n 's/^\(.*>\).*$/Signed-off-by: \1/p')
+ grep -qs "^$SOB" "$1" || echo "$SOB" >> "$1"
+
+# This example catches duplicate Signed-off-by lines.
+
+test "" = "$(grep '^Signed-off-by: ' "$1" |
+	 sort | uniq -c | sed -e '/^[ 	]*1[ 	]/d')" || {
+	echo >&2 Duplicate Signed-off-by lines.
+	exit 1
+}
