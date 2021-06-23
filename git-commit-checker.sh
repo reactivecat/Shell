a0ecfacdf463a50d46179821fa2b66bfcebbb4ef
gaizi <chenjianjun@yiwei.com>
2021-06-23 18:35:20 +0800

    ddd

--- git-commit-checker.sh
+++ git-commit-checker.sh
@@ -4,3 +4,5 @@ commit_msg="llalal,cmomit"
 echo "${commit_msg},melo girl!"
 echo "????$0 $1 $2 $3 hello"
 echo 'args:---- '$0''
+commit_message=$(cat "${1:?Missing commit message file}")
+echo  "content of commit_message: $commit_message"
