d72311a3410dbc72b216d2b9a59b2b4aebb7f0c3
gaizi <chenjianjun@yiwei.com>
2021-06-24 09:38:04 +0800

    commit message UTC3

--- git-commit-checker.sh
+++ git-commit-checker.sh
@@ -3,7 +3,7 @@ current_commit_message=`cat $1`
 #commit_messages=`git log --oneline`
 action=${current_commit_message:0:7}
 moduleName=${current_commit_message:7:9}
-echo "$current_commit_message"
+echo "message:>>>>>$current_commit_message"
 message=${current_commit_message:10:-1}
 echo "action:$action"
 echo 'message:'"$message"''
